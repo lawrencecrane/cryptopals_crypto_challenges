2d4e3e9afd12be6685bf3d0618e0950271e948b0
Lauri Kurki <lapeheku@student.jyu.fi>
2018-10-27 22:59:12 +0300

    repeating xor and dictionary attack

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.6-alpine
 WORKDIR /home/crypto
 
 RUN pip install --upgrade pip
-RUN pip install pytest
-RUN pip install hypothesis
+RUN pip install pytest hypothesis
+RUN apk add aspell aspell-en enchant && pip install pyenchant
 
 CMD ["sh"]
--- src/test_crypto.py
+++ src/test_crypto.py
@@ -14,3 +14,8 @@ def test_fixed_xor():
 def test_fixed_xor_idempotency(x):
     a = encode(bytearray(x, 'utf8'), 'hex')
     assert crypto.fixed_xor(a, a) == b'0' * len(a)
+
+def test_repeating_xor():
+    msg = b"""Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"""
+    assert crypto.repeating_xor(msg, b'ICE') == b'0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
